99d4b1fa341b9f94d89d86083ce350d75c0321ca
deVelop <deVelop>
2018-07-16 10:55:05 -0700

    form layout

--- app/react-app/src/App.js
+++ app/react-app/src/App.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { InputText } from 'primereact/inputtext';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div class="grid-container">
-          <div class="header">Header</div>
-          <div class="main">Main</div>
-          <div class="right">Right</div>
-          <div class="footer">Footer</div>
+          <div class="header">
+            <div class="headerText">Dashboard</div>
+          </div>
+          <div class="main">
+            <div class="middlecontainer">
+              <div class="panelheader">
+                Mobile Recharge
+              </div>
+              <div class="formcontainer">
+                <div class="inputgroup">
+                  <div class='formlabel'>Phone No.</div>
+                  <div class='forminput'><InputText /></div>
+                </div>
+                <div class="inputgroup">
+                  <div class='formlabel'>Operator</div>
+                  <div class='forminput'><InputText /></div>
+                </div>
+                <div class="inputgroup">
+                  <div class='formlabel'>Amount</div>
+                  <div class='forminput'><InputText /></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="right">
+            <div class="middlecontainer">
+            <div class="panelheader">
+              Mobile Recharge
+            </div>
+            <div class="formcontainer">
+              <div class="inputgroup">
+                <div class='formlabel'>Phone No.</div>
+                <div class='forminput'><InputText /></div>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="footer">
+            <div class="footercontainer"></div>
+          </div>
         </div>
       </div>
     );
